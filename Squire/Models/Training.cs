ceca62dde8ff8a4660efa74836ed363b2945f3b8
PavelShchetska <Pavel.Shchetska@gmail.com>
2015-04-03 07:02:53 -0700

    SBMT .NET: Added the model for the traning table

---
+++ Squire/Models/Training.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+
+namespace Squire.Models
+{
+    public class Training : IHttpModule
+    {
+        public int trainingID { get; set; }
+        public string trainingName { get; set; }
+        public string trainingDescription { get; set; }
+        public string units { get; set; }
+        public decimal price { get; set; }
+        public decimal trainer { get; set; }
+    }
+}
