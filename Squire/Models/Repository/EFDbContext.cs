98de1dbb2322d4eca81cd2a115b276e7310521ab
PavelShchetska <Pavel.Shchetska@gmail.com>
2015-04-05 13:35:49 -0700

    SBMT .NET: Added the entityFramework

---
+++ Squire/Models/Repository/EFDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Squire.Models.Repository
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Training> training { get; set; }
+    }
+}
